2873d3a1becc00b345d1a6bbaa460e38dcb399d8
Vesemi <78653685+Vesemi@users.noreply.github.com>
2022-06-15 20:14:47 +0200

    Added Basic Database Management

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import sqlite3;
 from PySide6.QtGui import QPalette, QColor, QFont
 from PySide6.QtWidgets import *
+from matplotlib import projections
 import data_management
 
 
@@ -10,7 +11,15 @@ end_date = 2
 project_nr = 6     # rowne 1 gdy przyklady usuniete
 colors = ['yellow', 'white', 'blue', 'red', 'pink', 'purple']
 
-Baza = data_management.Database('banana')
+baza = data_management.Database('database')
+baza.drop_table('projects')
+baza.drop_table('employees')
+baza.drop_table('tasks')
+
+baza.create_table("employees(id INTEGER PRIMARY KEY AUTOINCREMENT, name text, department text, position text, tasks int)")
+baza.create_table("projects(id INTEGER PRIMARY KEY AUTOINCREMENT, title text, description text, startDate text, finishDate text, urgency int, allTasks int, doneTasks int)")
+baza.create_table("tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, projectId int, employe int, title text, description text, urgency int, status int, startDate text, finishDate text, creationDate text)")
+
 
 
 
@@ -25,6 +34,15 @@ class Color(QWidget):
 
 
 class MainWindow(QMainWindow):
+    def add_project(self):
+        baza.addProject(('Project1', 'Project one long description', '2022-06-14', '', 1, 0, 0))
+    
+    def add_task(self):
+        baza.addTask(('Project1', 'Project one long description', '2022-06-14', '', 1, 0, 0))
+    
+    def add_employe(self):
+        baza.addEmploye(('Jan', 'Kowalski', 'IT', 'Dev'))
+    
     def __init__(self):
         super().__init__()
 
@@ -33,12 +51,41 @@ class MainWindow(QMainWindow):
 
         global tabs, tab1, tab2, left_panel, right_panel
         tabs = QTabWidget()
-        tab1 = QTabWidget()
-        tab2 = QTabWidget()
+        tab1 = QTabWidget() #Progress Tab
+        tab1_2 = QTabWidget() #Progress Tab Single Project
+        tab2 = QTabWidget() #Project Management tab
+        tab2_2 = QTabWidget() #Project Management tab
+        tab3 = QTabWidget() #Employes tab
+        
+        buttonAddProject = QPushButton('Add Project')
+        buttonAddProject.clicked.connect(lambda: self.add_project())
+
+        buttonAddEmploye = QPushButton('Add Employe')
+        buttonAddEmploye.clicked.connect(lambda: self.add_employe())
+
+        buttonAddTask = QPushButton('Add Task')
+        buttonAddTask.clicked.connect(lambda: self.add_task())
+        
         tabs.setTabPosition(QTabWidget.North)
         tabs.setMovable(True)
         tabs.addTab(tab1, "Main")
+        tabs.addTab(tab1_2, "Main2")
         tabs.addTab(tab2, "Add Project")
+        tabs.addTab(tab2_2, "Add Project2")
+        tabs.addTab(tab3, "Użytkownicy")
+        
+           
+        projects = QHBoxLayout()
+        projectsLeft = QHBoxLayout()
+        projectsRight = QHBoxLayout()
+        projectsLeft.addWidget(buttonAddEmploye)
+        projectsRight.addWidget(buttonAddProject)
+        projectsRight.addWidget(buttonAddTask)
+
+        projects.addLayout(projectsLeft)
+        projects.addLayout(projectsRight)
+        tab2_2.setLayout(projects)     
+        
         layout = QHBoxLayout()
         left_panel = QGridLayout()
         header = QLabel("Project name")
@@ -74,10 +121,9 @@ class MainWindow(QMainWindow):
         left_panel.addWidget(QLabel("Projekt nr 4"), 5, 0)               #
         for i in range(4, 25):                                           # do usuniecia, tylko przyklad
             right_panel.addWidget(Color('purple'), 5, i)                 #
-
+   
         layout.addLayout(left_panel)
         layout.addLayout(right_panel)
-
         tab1.setLayout(layout)
 
         lista_dni = []
@@ -100,6 +146,7 @@ class MainWindow(QMainWindow):
         layout2.addWidget(self.combobox1, 1, 1)
         layout2.addWidget(self.combobox2, 2, 1)
         layout2.addWidget(self.button1, 1, 3)
+
         tab2.setLayout(layout2)
 
         self.button1.setCheckable(True)
